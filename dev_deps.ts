8e2941e4966d0cb80799b2849ca6bcf20eccb450
kamilernerd <kamil.oracz98@gmail.com>
2023-01-07 00:24:59 +0100

    fix??

--- dev_deps.ts
+++ dev_deps.ts
@@ -3,20 +3,18 @@ export {
   AssertionError,
   assertNotEquals,
   assertRejects,
-  assertThrows,
-} from "https://deno.land/std@0.116.0/testing/asserts.ts";
+  assertThrows
+} from "https://deno.land/std@0.171.0/testing/asserts.ts";
 
-export { exists } from "https://deno.land/std@0.116.0/fs/exists.ts";
+export { exists } from "https://deno.land/std@0.171.0/fs/exists.ts";
 
-export { ensureDir } from "https://deno.land/std@0.116.0/fs/ensure_dir.ts";
+export { ensureDir } from "https://deno.land/std@0.171.0/fs/ensure_dir.ts";
 
 export {
   dirname,
-  fromFileUrl,
-} from "https://deno.land/std@0.116.0/path/mod.ts";
+  fromFileUrl
+} from "https://deno.land/std@0.171.0/path/mod.ts";
 
-export {
-  MuxAsyncIterator,
-} from "https://deno.land/std@0.116.0/async/mux_async_iterator.ts";
+export { MuxAsyncIterator } from "https://deno.land/std@0.171.0/async/mux_async_iterator.ts";
 
-export { stripColor } from "https://deno.land/std@0.116.0/fmt/colors.ts";
+export { stripColor } from "https://deno.land/std@0.171.0/fmt/colors.ts";
